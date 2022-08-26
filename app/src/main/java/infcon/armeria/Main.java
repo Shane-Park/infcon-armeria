30651306441719cb3bf450447579077062657dd0
shane <psh40963@naver.com>
2022-08-26 16:06:26 +0900

    frontend

--- app/src/main/java/infcon/armeria/Main.java
+++ app/src/main/java/infcon/armeria/Main.java
@@ -1,8 +1,19 @@
 package infcon.armeria;
 
+import com.linecorp.armeria.server.Server;
+import com.linecorp.armeria.server.ServerBuilder;
+
+import java.util.concurrent.CompletableFuture;
+
 public final class Main {
 
     public static void main(String[] args) {
+        ServerBuilder serverBuilder = Server.builder();
+        Server server = serverBuilder.http(8080)
+                .service("/infcon", new MyService())
+                .build();
 
+        CompletableFuture<Void> future = server.start();
+        future.join();
     }
 }
